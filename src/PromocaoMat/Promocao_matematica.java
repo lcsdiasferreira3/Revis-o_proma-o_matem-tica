162138e0a95be489838fc0118dacafdddb9d097b
unknown <lcsdiasferreira3>
2018-11-27 21:29:26 -0200

    revisão

---
+++ src/PromocaoMat/Promocao_matematica.java
@@ -0,0 +1,23 @@
+package PromocaoMat;
+
+public class Promocao_matematica {
+	public static void main(String[] args) {
+		
+double  d = 100.99;
+int i = 100;
+
+ d = d * i;
+ 
+ long x =  12345; 
+ float pi  = 3.14f ;
+ x = x * (long) pi;
+ x = (long) (x * pi);
+ 
+ 	System.out.println(x);
+ 	System.out.println(d);
+ 	System.out.println(pi);
+		
+		 
+	}
+
+}
